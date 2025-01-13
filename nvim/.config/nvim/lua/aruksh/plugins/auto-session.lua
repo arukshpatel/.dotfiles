78608cecad76d9f2b69075f9fc256dab9d9006c6
Aruksh Patel <arukshpatel98@gmail.com>
2025-01-13 15:27:41 -0600

    added nvim plugins and modified ghostty

--- nvim/.config/nvim/lua/aruksh/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/aruksh/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
 
         auto_session.setup({
             auto_restore_enabled = true,
-            -- auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop" },
+            auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop" },
         })
 
         local keymap = vim.keymap
